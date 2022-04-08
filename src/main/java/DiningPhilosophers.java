972da2bb4eb3ab21ffb3619fad8fd15834af0e7f
Sindre Skorpen <64530905+Westdespair@users.noreply.github.com>
2022-04-08 11:51:44 +0200

    Fixed multithreading functionality for the main class. Philosophers are now runnable, and can be hungry, thinking, and dead.

--- src/main/java/DiningPhilosophers.java
+++ src/main/java/DiningPhilosophers.java
@@ -1,11 +1,16 @@
 import java.util.concurrent.Executor;
 import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class DiningPhilosophers extends Thread {
 
     public static void main(String[] args) {
+        int diners = 10;
+        ExecutorService threadExecutor = Executors.newFixedThreadPool(diners);
         DiningPhilosophers diningPhilosophers = new DiningPhilosophers();
-        DiningTable diningTable = new DiningTable(1);
-        diningTable.getPhilosopher(0).run();
+        DiningTable diningTable = new DiningTable(diners);
+        for (int i = 0; i < diners; i++) {
+            threadExecutor.execute(diningTable.getPhilosopher(i));
+        }
     }
 }
--- src/main/java/Philosopher.java
+++ src/main/java/Philosopher.java
@@ -4,27 +4,30 @@ public class Philosopher implements Runnable {
     private int starvationLimit = 100;
     private int starvationRate = 1;
     private int eatingRate = 1;
-    private boolean isAlive;
-    private boolean hasLeftChopstick;
-    private boolean hasRightChopstick;
+    private boolean isAlive = true;
     private enum State {THINKING, EATING, HUNGRY, DEAD};
     private State currentState = State.THINKING;
-    private static String[] nameList = new String[]{"Plato", "Aristotle", "Diogenes", "Nietsche", "Confusius",
+    private final String[] nameList = new String[]{"Plato", "Aristotle", "Diogenes", "Nietsche", "Confusius",
             "Sigmund Freud", "Immanuel Kant", "Rene Descartes", "Immanuel Kant", "Sindre", "Malin", "Jonas" };
 
+    /**
+     * Makes the philosopher act, as long as it is still alive
+     */
     @Override
     public void run() {
         while (isAlive) {
+            State previousState = currentState;
+            act();
+            if(currentState != previousState) {
+                printStatus();
+            }
             try {
-                wait(100);
+                Thread.sleep(200);
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
-            setCurrentState();
-            printStatus();
         }
     }
-
 
     /**
      * Creates an instance of the Philosopher class. A name is selected from the index.
@@ -69,6 +72,7 @@ public class Philosopher implements Runnable {
     private void act() {
         //Hunger is always growing
         starve();
+        setCurrentState();
         switch(currentState) {
             case THINKING:
                 break;
@@ -78,6 +82,9 @@ public class Philosopher implements Runnable {
 
             case EATING:
                 eat();
+                if(starvation == 0) {
+
+                }
                 break;
 
             case DEAD:
@@ -86,15 +93,39 @@ public class Philosopher implements Runnable {
         }
     }
 
+    /**
+     * sets the current state of the philosopher, based on their hunger level.
+     */
     private void setCurrentState() {
         int hungerLimit = (int)(starvationLimit/2);
-        currentState = State.THINKING;
-            if(hungerLimit < starvation) {
+
+            if(hungerLimit < starvation && currentState != State.EATING) {
                 currentState = State.HUNGRY;
 
                 if(starvationLimit <= starvation) {
                     currentState = State.DEAD;
                 }
+
+            } else {
+                currentState = State.THINKING;
+            }
+        }
+
+        public void forceState(State state) {
+            switch(state) {
+                case THINKING:
+                    currentState = State.THINKING;
+                    break;
+                case HUNGRY:
+                    currentState = State.HUNGRY;
+                    break;
+
+                case EATING:
+                    currentState = State.EATING;
+
+                case DEAD:
+                    currentState = State.DEAD;
+                    break;
             }
         }
 
@@ -109,13 +140,13 @@ public class Philosopher implements Runnable {
      * The philosopher starves, increasing their starvation based on the starvationrate.
      */
     public void starve() {
-        starvation =+ starvationRate;
+        starvation += starvationRate;
     }
 
     /**
      * The philosopher eats, reducing their starvation based on their eatingrate.
      */
     public void eat() {
-        starvation =- eatingRate;
+        starvation -= eatingRate;
     }
 }
